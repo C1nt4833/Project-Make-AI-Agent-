458e56bca375b279ff36270b9259a207cc8663fb
Vyo <vyolasesilia@gmail.com>
2026-05-11 19:04:30 +0800

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 // Konfigurasi API
-const API_URL = 'http://127.0.0.1:5000/analyze';
+const API_URL = 'https://project-make-ai-agent.onrender.com/analyze';
 
 // Global Variables
 let stream = null;
